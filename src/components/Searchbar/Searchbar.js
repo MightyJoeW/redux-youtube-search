ae3f41aa2b3352ccf9bc979ff2d37dc708efb13d
Joe Warren <joe.warren7@gmail.com>
2018-03-10 00:07:24 -0600

    chore: update config and move onInputChange above render

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,20 +1,18 @@
 import React, { Component } from "react";
 export default class Searchbar extends Component {
+  onInputChange(e) {
+    console.log(e.target.value);
+  }
+
   render() {
     return (
       <div style={{ gridColumn: "1 / -1", justifySelf: "center" }}>
         <input
-          style={{
-            borderRadius: "50px",
-            width: "500px"
-          }}
+          style={{ borderRadius: "50px", width: "500px" }}
           onChange={this.onInputChange}
           placeholder="search"
         />
       </div>
     );
-  }
-  onInputChange(e) {
-    console.log(e.target.value);
   }
 }
