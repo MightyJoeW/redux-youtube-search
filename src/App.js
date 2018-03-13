887e4c6426fe93a43f0affb8a807b010d670a9eb
Joe Warren <joe.warren7@gmail.com>
2018-03-12 22:31:01 -0500

    chore: log a test search with youtube-api-search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import YTSearch from "youtube-api-search";
+import { API_Key } from "./config";
 
 // COMPONENTS
 import SearchBar from "./components/SearchBar/SearchBar";
 import VideoDetail from "./components/VideoDetail/VideoDetail";
 import VideoList from "./components/VideoList/VideoList";
 import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
+
+YTSearch({ key: API_Key, term: "nba" }, function(data) {
+  console.log(data);
+});
 
 export default class App extends Component {
   constructor(props) {
