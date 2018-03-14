80cfaad59fde63eefc503df8c69f0b579cde0349
Joe Warren <joe.warren7@gmail.com>
2018-03-14 13:51:45 -0500

    chore: display video thumbnail for each ListItem

--- src/components/VideoListItem/VideoListItem.js
+++ src/components/VideoListItem/VideoListItem.js
@@ -4,18 +4,24 @@ import {ListItem} from 'material-ui/List';
 
 // Local Variables
 const listGroupItem = {};
-const videoList = {}
-const mediaLeft = {}
-const mediaObject = {}
+const videoList = {};
+const mediaLeft = {};
+const mediaObject = {};
+const mediaBody = {};
+const mediaHeading = {};
 
 // Component Definition
 const VideoListItem = ({video}) => {
   // ES6 shorthand for taking in props and doing const video = props.video
+  const imageUrl = video.snippet.thumbnails.default.url;
   return (
       <ListItem style={listGroupItem}>
         <div style={videoList}>
           <div style={mediaLeft}>
-            <img style={mediaObject}/>
+            <img style={mediaObject} src={imageUrl}/>
+          </div>
+          <div style={mediaBody}>
+            <div style={mediaHeading}></div>
           </div>
         </div>
       </ListItem>
