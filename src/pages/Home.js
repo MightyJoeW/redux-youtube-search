8b85e9b8c5bc871b64bc033922100057d180f23b
Joe Warren <joe.warren7@gmail.com>
2018-03-25 22:01:35 -0500

    fix: unneeded mui v0 lines from app to remove errors, add mui appbar and react-autosuggest

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import { API_Key } from '../config';
 import YTSearch from 'youtube-api-search';
 
 // Internal Dependencies
-import SearchBar from '../components/Searchbar/SearchBar';
+import SearchBar from '../components/SearchBar/SearchBar';
 import VideoDetail from '../components/VideoDetail/VideoDetail';
 import VideoList from '../components/VideoList/VideoList';
 import VideoPlayer from '../components/VideoPlayer/VideoPlayer';
@@ -37,7 +37,13 @@ export default class componentName extends Component {
     }
     render() {
         return (
-            <div>
+            <div
+                style={{
+                    display: 'grid',
+                    gridGap: '1em',
+                    gridTemplateColumns: '2fr 1fr',
+                    margin: '2em 3em 0 3em',
+                }}>
                 <SearchBar change={this.handleChange} onChange={this.handleChange} />
                 <VideoPlayer />
                 <VideoList videos={this.state.videos} />
