af706e4584afc7211e670e0afdcd459e50d8c5f4
Joe Warren <joe.warren7@gmail.com>
2018-03-27 09:54:03 -0500

    chore: render video title and viceo channel

--- src/components/VideoListItem/VideoListItem.js
+++ src/components/VideoListItem/VideoListItem.js
@@ -1,19 +1,32 @@
 // External Dependencies
 import React from 'react';
-import {ListItem} from 'material-ui/List';
+import { ListItem } from 'material-ui/List';
 
 // Local Variables
-const listGroupItem = {};
-const videoList = {};
+const listGroupItem ={};
+const videoList = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(2, 1fr)'
+};
 const mediaLeft = {};
 const mediaObject = {};
 const mediaBody = {};
-const mediaHeading = {};
+const mediaHeading = {
+  fontSize: '0.8em',
+  fontWeight: 700
+};
+const mediaChannel = {
+  color: '#8A8A8A',
+  fontSize: '0.7em'
+}
 
 // Component Definition
 const VideoListItem = ({video}) => {
   // ES6 shorthand for taking in props and doing const video = props.video
+  const channelTitle = video.snippet.channelTitle;
   const imageUrl = video.snippet.thumbnails.default.url;
+  const title = video.snippet.title;
+  console.log(video);
   return (
       <ListItem style={listGroupItem}>
         <div style={videoList}>
@@ -21,7 +34,8 @@ const VideoListItem = ({video}) => {
             <img style={mediaObject} src={imageUrl}/>
           </div>
           <div style={mediaBody}>
-            <div style={mediaHeading}></div>
+            <div style={mediaHeading}>{title}</div>
+            <div style={mediaChannel}>{channelTitle}</div>
           </div>
         </div>
       </ListItem>
